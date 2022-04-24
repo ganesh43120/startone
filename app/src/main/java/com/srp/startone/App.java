99d09f8579813bfa027c31ae4668a3574074d70f
Ganesh <kurakuganesh123@gmail.com>
2022-04-24 23:21:49 +0530

    starting stage of project

---
+++ app/src/main/java/com/srp/startone/App.java
@@ -0,0 +1,13 @@
+package com.srp.startone;
+
+import android.app.Application;
+import android.content.Intent;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        startService(new Intent(this, YourService.class));
+    }
+}
--- app/src/main/java/com/srp/startone/MainActivity.java
+++ app/src/main/java/com/srp/startone/MainActivity.java
@@ -1,7 +1,15 @@
 package com.srp.startone;
 
-import android.content.DialogInterface;
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.telephony.SmsManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +19,8 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,6 +29,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String CHANNEL_ID = "personal_notification";
+    public static final int NOTIFICATION_ID = 101;
+
     EditText phonenumber, message;
     Button book;
     // creating a variable for
@@ -32,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
     // variable for Text view.
     private TextView retrieveTV;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,46 +62,54 @@ public class MainActivity extends AppCompatActivity {
         retrieveTV = findViewById(R.id.idTVRetrieveData);
 
         // calling method
-        // for getting data.
+        // for getting data
         getdata();
-        if (retrieveTV.getText() == "296") {
-            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-            builder.setCancelable(true);
-            builder.setTitle("gas is running low");
-            builder.setMessage("do want book now");
-            builder.setNegativeButton("cacel", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    dialogInterface.cancel();
-                }
-            });
-            builder.setPositiveButton("book now", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
+    }
+    public void createNotificationchannel() {
 
-                }
-            });
-            book = findViewById(R.id.button);
-            phonenumber = findViewById(R.id.editText);
-            message = findViewById(R.id.editText2);
-            book.setOnClickListener(view -> {
-                String number = phonenumber.getText().toString();
-                String msg = message.getText().toString();
-                try {
-                    SmsManager smsManager = SmsManager.getDefault();
-                    smsManager.sendTextMessage(number, null, msg, null, null);
-                    Toast.makeText(getApplicationContext(), "Message Sent", Toast.LENGTH_LONG).show();
-                } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), "Some fields are Empty", Toast.LENGTH_LONG).show();
-                }
-            });
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Personal Notifications";
+            String description = "Include all notification";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+
+            notificationChannel.setDescription(description);
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(notificationChannel);
         }
+    }
+    public void headnote(){
+        createNotificationchannel();
+            Intent landingingintent = new Intent(this,MainActivity.class);
+            landingingintent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+            PendingIntent landingPendingIntent = PendingIntent.getActivity(this,0,landingingintent,PendingIntent.FLAG_ONE_SHOT);
+
+            Intent yesIntent = new Intent(this,MainActivity.class);
+            yesIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent yesPendingIntent = PendingIntent.getActivity(this,0,yesIntent,PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.ic_sms_notification);
+        builder.setContentTitle("simple Notification");
+        builder.setContentText("this is notification");
+        builder.setPriority(NotificationCompat.PRIORITY_HIGH);
+        builder.setAutoCancel(true);
+        builder.setContentIntent(landingPendingIntent);
+        builder.addAction(R.drawable.ic_sms_notification,"yes",yesPendingIntent);
+        //builder.addAction(R.drawable.ic_sms_notification,"no",noPendingIntent);
+        Toast.makeText(getApplicationContext(), "action required", Toast.LENGTH_LONG).show();
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(NOTIFICATION_ID,builder.build());
     }
 
     private void getdata() {
         // calling add value event listener method
         // for getting the values from database.
         databaseReference.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 // this method is call to get the realtime
@@ -97,10 +119,28 @@ public class MainActivity extends AppCompatActivity {
                 // below line is for getting the data from
                 // snapshot of our database.
                 String value = snapshot.getValue(String.class);
-
                 // after getting the value we are setting
                 // our value to our text view in below line.
                 retrieveTV.setText(value);
+                assert value != null;
+                if (value.equals("296")) {
+                    retrieveTV.setText("96");
+                    headnote();
+                    book = findViewById(R.id.button);
+                    phonenumber = findViewById(R.id.editText);
+                    message = findViewById(R.id.editText2);
+                    book.setOnClickListener(view -> {
+                        String number = phonenumber.getText().toString();
+                        String msg = message.getText().toString();
+                        try {
+                            SmsManager smsManager = SmsManager.getDefault();
+                            smsManager.sendTextMessage(number, null, msg, null, null);
+                            Toast.makeText(getApplicationContext(), "Message Sent", Toast.LENGTH_LONG).show();
+                        } catch (Exception e) {
+                            Toast.makeText(getApplicationContext(), "Some fields are Empty", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
             }
 
             @Override
